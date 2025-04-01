1571abe736c96fb711e45553006340f79f86219e
Sasmita Sahoo <lizasasmitasahoo83@gmail.com>
2025-04-01 16:02:36 +0000

    variable changes

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 14443
+let accountEmail = "lizasasmitasahoo@gmail.com"
+var accountPassword = "12345"
+accountCity = "Keonkjhar"//variable can declare like this
+let accountState;
+
+
+// accountId = 2  //not allowed bcz it is used  const
+// variable written is of 2 type one is let and var.
+
+accountEmail ="hc2hc.com"
+accountPassword = "1234555"
+accountCity = "Bhibaneswar"
+
+console.log(accountId);
+
+/* Prefer not to use var 
+bcs of issue in block scope and function*/
+console.table([accountEmail,accountPassword,accountCity,accountState]);
