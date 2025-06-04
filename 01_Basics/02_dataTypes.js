366efe328d68532f661dea0c7f116a1b0f502f66
Sasmita Sahoo <lizasasmitasahoo83@gmail.com>
2025-06-04 05:44:25 +0000

    DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all code as newee version
+
+//alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+console.log("Sasmita")
+
+
+
+let name="hitesh"
+let age=22
+let isLoggedIn=false
+
+
+//number => 2 to the power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value//let state=null =empty
+//undefdined 
+//symbol => unique
+
+//object
+
+console.log(typeof null); //null is a object
+console.log(typeof undefined);//undefned Type
