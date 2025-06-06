7550336148640f41303dc6261f27524066bd9ba3
Sasmita Sahoo <lizasasmitasahoo83@gmail.com>
2025-06-06 07:26:44 +0000

    Conversionof datatypes

--- 01_Basics/01_variable.js
+++ 01_Basics/01_variable.js
@@ -1,19 +1,23 @@
 const accountId = 14443
 let accountEmail = "lizasasmitasahoo@gmail.com"
 var accountPassword = "12345"
-accountCity = "Keonkjhar"//variable can declare like this
-let accountState;
+accountCity = "Keonkjhar"//variable can declare like this without use var let
+let accountState; //not value assign 
 
 
 // accountId = 2  //not allowed bcz it is used  const
 // variable written is of 2 type one is let and var.
 
+/* Prefer not to use var always use let 
+bcs of issue in block scope and function*/
+
 accountEmail ="hc2hc.com"
-accountPassword = "1234555"
+accountPassword = "1234555"   
 accountCity = "Bhibaneswar"
 
 console.log(accountId);
 
-/* Prefer not to use var 
-bcs of issue in block scope and function*/
-console.table([accountEmail,accountPassword,accountCity,accountState]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+//Scope{}
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="sasmita"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score) // 33-string to number
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//33 - 33
+//33abc => Nota number (Nan)
+//true => 1;false=> 0
+
+let isLoggedIn = "sasmita"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//1 => true; 0=> false 
+//"" => false
+//"sasmita"=> true
+
+let someNumber=33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
