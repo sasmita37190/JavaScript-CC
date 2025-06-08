be0620227d48f3a35be45bf7eb1ae60dfbcd3417
Sasmita Sahoo <lizasasmitasahoo83@gmail.com>
2025-06-08 15:11:02 +0000

    comparission operator

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+
+// console.log("2">1);   //automatically js  convert at number
+// console.log("02">1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);   
+
+//comparision convert  null toa number treating it as 0 that  is why null>=0  true and null>0 is false.
+ //console.log(undefined == 0);
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+//console.log(undefined >= 0);
+ //console.log(undefined <= 0);
+
+
+// ===
+
+console.log("2" === 2);
